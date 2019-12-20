fb4c167fbf5a8f10327401b7d3e09e40cb49c20d
Sean Doyle <sdoylelambda@gmail.com>
2019-12-20 11:47:54 -0500

    Sudo code and UPER framework for ring buffer.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,8 +6,27 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        # UPER
+        # U - UNDERSTAND
+        #     ring buffer is a non-growable buffer with a fixed size
+        #     When the ring buffer is full and a new element is inserted,
+        #     the oldest element in the ring buffer is overwritten with the newest element
 
     def append(self, item):
+        # DO FIRST - SO I CAN HAVE SOMETHING TO get
+        # P - Plan
+        if value < self.value:
+        #     append adds item to ring buffer
+        #     if ring buffer is full:         =====> $$$$$ fix value for at_capacity $$$$$$$
+            at_capacity = True
+            if at_capacity == True:
+        #         oldest element in ring = item
+                self.storage[0] == item
+            else:
+        #         add item to ring buffer
+                self.storage.insert_after(item)
+        #
+
         pass
 
     def get(self):
@@ -15,8 +34,62 @@ class RingBuffer:
         list_buffer_contents = []
 
         # TODO: Your code here
+        # AFTER APPEND TESTS PASS
+
 
         return list_buffer_contents
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # ----------------Stretch Goal-------------------
 
